8b909f9cc76a4d2fe9749a639bbd1a46228d3808
Alexander Koskovich <akoskovich@pm.me>
2023-12-01 03:55:35 +0530

    sm8250-common: Drop updatable GPU drivers
    * Sony is using the qcom empty stub which apparently isn't even on
      Play Store
    
    Change-Id: Id009316bd38ca126e7223a6deaae3c9ddca5298f

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -1657,7 +1657,6 @@ PRODUCT_PACKAGES += \
     CneApp \
     IWlanService \
     TimeService \
-    com.qualcomm.qti.gpudrivers.kona.api30 \
     HotwordEnrollmentOKGoogleHEXAGON \
     HotwordEnrollmentXGoogleHEXAGON \
     ImsRcsService \
